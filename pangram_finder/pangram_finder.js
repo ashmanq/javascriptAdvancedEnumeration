6d259cebbb251a996d3cf156e55cb4ee047d909f
Ashir <ashirq@outlook.com>
2020-04-15 19:48:28 +0100

    All test pass for pangram finder.

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,30 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.split('');
 }
 
-PangramFinder.prototype.isPangram = function () {
+PangramFinder.prototype.sortPhrase = function (phrase) {
 
+};
+PangramFinder.prototype.isPangram = function () {
+  // const stringArray =
+  const phraseSpacesFilteredOut = this.phrase.filter((letter) => {
+    return letter != ' ';
+  })
+
+  const phraseToCompare = phraseSpacesFilteredOut.map((letter) => {
+    return letter.toLowerCase();
+  })
+
+  const result_array = this.alphabet.map((letter) => {
+    return phraseToCompare.indexOf(letter);
+    });
+
+    const isItPangram = result_array.every((result) => {
+      return result !== -1;
+    })
+
+ return isItPangram;
 }
 
 module.exports = PangramFinder;
