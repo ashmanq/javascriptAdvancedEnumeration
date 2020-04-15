9c447968b9a17aac3aca2a7440a4b75d8fcc4085
Ashir <ashirq@outlook.com>
2020-04-15 20:09:06 +0100

    Isogram app now passing all tests.

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,18 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
 
+  const wordLowerCase = this.word.map((letter) => {
+    return letter.toLowerCase();
+  })
+
+  const result = wordLowerCase.every((letter, i) => {
+      return wordLowerCase.indexOf(letter) === i;
+  });
+
+  return result;
 }
 
 module.exports = IsogramFinder;
