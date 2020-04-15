dcc211cced6a9cf40fa23614868f0929e0b2a528
Ashir <ashirq@outlook.com>
2020-04-15 18:09:57 +0100

    upper case code tested ok

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,12 @@
 const UpperCaser = function (words) {
+  this.words = words;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
- return ['SHOUTING']
+ const upperCasedWords = this.words.map( (word) => {
+   return word.toUpperCase();
+ })
+ return upperCasedWords;
 }
 
 module.exports = UpperCaser;
